90eb60881e64ac9fe11ec156712054454f4235a1
Yaolin_Li <we1586505611@163.com>
2021-09-08 13:56:26 +0800

    1.Add ReversePartition() Function, which is used to reverse the linked list from the ath to the bth node.
    2.Added test program for function Reverse().

--- experiment/linkedlist/singly_linkedlist/singlylinkedlist.go
+++ experiment/linkedlist/singly_linkedlist/singlylinkedlist.go
@@ -1,6 +1,9 @@
 package singly_linkedlist
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type snode struct {
 	Val  interface{}
@@ -94,6 +97,38 @@ func (ll *SingleLinkedList) Reverse() {
 	ll.Head = prev
 }
 
+// ReversePartition Reverse the linked list from the ath to the bth node
+func (ll *SingleLinkedList) ReversePartition(left, right int) error {
+	err := ll.CheckRangeFromIndex(left, right)
+	if err != nil {
+		return err
+	}
+	tmpNode := NewNode(-1)
+	tmpNode.Next = ll.Head
+	pre := tmpNode
+	for i := 0; i < left-1; i++ {
+		pre = pre.Next
+	}
+	cur := pre.Next
+	for i := 0; i < right-left; i++ {
+		next := cur.Next
+		cur.Next = next.Next
+		next.Next = pre.Next
+		pre.Next = next
+	}
+	ll.Head = tmpNode.Next
+	return nil
+}
+func (ll *SingleLinkedList) CheckRangeFromIndex(left, right int) error {
+	if left > right {
+		return errors.New("left boundary must smaller than right")
+	} else if left < 1 {
+		return errors.New("left boundary starts from the first node")
+	} else if right > ll.length {
+		return errors.New("right boundary cannot be greater than the length of the linked list")
+	}
+	return nil
+}
 func (ll *SingleLinkedList) Display() {
 	for cur := ll.Head; cur != nil; cur = cur.Next {
 		fmt.Print(cur.Val, " ")
